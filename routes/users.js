b0130ba4a60976a29337947e5ccecda098301199
nikithasri-168 <nikithasrigarapati@gmail.com>
2024-03-20 17:06:22 -0500

    c2

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+let product=1;
+let factor=1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  factor++;
+  product=product*factor;
+  res.send(`product is ${product}`);
 });
 
 module.exports = router;
