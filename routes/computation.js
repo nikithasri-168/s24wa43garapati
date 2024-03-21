b0a47332fcf290363cf748b1ba1aaf1226b279a0
nikithasri-168 <nikithasrigarapati@gmail.com>
2024-03-20 21:33:32 -0500

    bonus added

--- routes/computation.js
+++ routes/computation.js
@@ -5,7 +5,9 @@ let x = Math.random()*100;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   let y = Math.log10(x);
-  res.send(`log10 of ${x} is ${y}`);
+  let z = Math.imul(3, 4); 
+  let w = Math.log(x); 
+  res.send(`log10 of ${x} is ${y}. Math.imul(3, 4) = ${z}. Math.log(${x}) = ${w}`);
 });
 
 module.exports = router;
